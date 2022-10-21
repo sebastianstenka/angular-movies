b25d41bb8f42460a6ec5263f12b1d4f243546306
Sebastian Stenka <sebastian.stenka@dgt.eu>
2022-10-21 17:32:43 +0200

    Create pages/movies and pages/home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    MoviesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
