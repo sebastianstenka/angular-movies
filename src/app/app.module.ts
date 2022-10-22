fdf753b623fc40b18de3766ccbf11ec261d043e2
Sebastian Stenka <sebastian.stenka@dgt.eu>
2022-10-22 13:01:19 +0200

    Create slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { MoviesComponent } from './pages/movies/movies.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent, MoviesComponent, HomeComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, MoviesComponent, HomeComponent, SliderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
