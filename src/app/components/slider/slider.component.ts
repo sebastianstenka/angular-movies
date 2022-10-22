7574f1205f42977653d26d72c66ef5aaa524c05d
Sebastian Stenka <sebastian.stenka@dgt.eu>
2022-10-22 16:17:37 +0200

    Use api data to show sliders

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'slider',
@@ -6,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
-  @Input() items: any;
+  @Input() movies: Movie[] = [];
 
   constructor() {}
 
---
+++ src/app/models/movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  backdrop_path: string;
+  title: string;
+  release_date: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+}
