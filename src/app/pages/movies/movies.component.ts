5a27bfb47cdbe9c866b1f98498b2ef068ae6cabc
Sebastian Stenka <sebastian.stenka@dgt.eu>
2022-10-23 14:44:54 +0200

    Use paginator from primeng

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -10,12 +10,25 @@ import { MoviesService } from 'src/app/services/movies.service';
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
+  page: number = 1;
+  rows: number = 12;
 
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.moviesService.getMoviesByPage(MOVIES_GROUPS.popular, 1).subscribe((movies) => {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.moviesService.getMoviesByPage(MOVIES_GROUPS.popular, this.page).subscribe((movies) => {
       this.movies = movies;
     });
+  }
+
+  paginate(event: any) {
+    this.page = event.page + 1;
+    this.rows = event.rows;
+
+    this.getMovies();
   }
 }
